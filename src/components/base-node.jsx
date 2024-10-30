5b52d109093d2b7c0e309915a3b4f45d3ee4a556
shreyas0924 <shreyasp0924@gmail.com>
2024-10-30 07:42:35 +0530

    sizing done

--- src/components/base-node.jsx
+++ src/components/base-node.jsx
@@ -1,5 +1,28 @@
 import { useState } from "react";
-import { Handle, NodeResizer } from "reactflow";
+import { Handle, NodeResizeControl, NodeResizer } from "reactflow";
+
+function ResizeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="#ff0071"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      style={{ position: "absolute", right: 5, bottom: 5 }}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <polyline points="16 20 20 20 20 16" />
+      <line x1="14" y1="14" x2="20" y2="20" />
+      <polyline points="8 4 4 4 4 8" />
+      <line x1="4" y1="4" x2="10" y2="10" />
+    </svg>
+  );
+}
 
 export const BaseNode = ({
   id,
@@ -24,8 +47,13 @@ export const BaseNode = ({
     setFieldValues((prev) => ({ ...prev, [name]: value }));
   };
 
+  const controlStyle = {
+    background: "transparent",
+    border: "none",
+  };
+
   return (
-    <div className="rounded-lg p-6 border-[2px] border-[#A7AAD5] shadow-xl text-gray-800 w-full resizable-node">
+    <div className="rounded-lg p-6 border-[3px] border-[#A7AAD5] shadow-xl text-gray-800 w-full resizable-node">
       <div className="flex flex-col gap-2 border-b border-[#5E3A9D] pb-4 mb-4">
         <span className="text-lg font-semibold">{label}</span>
         <span className="text-sm text-gray-800">{description}</span>
@@ -48,7 +76,7 @@ export const BaseNode = ({
               </select>
             ) : field.type === "textarea" ? (
               <textarea
-                className="w-full mt-1 rounded-lg p-2 -mx-1 bg-transparent border border-gray-300 resize-none"
+                className="w-full min-h-full mt-1 rounded-lg p-2 -mx-1 bg-transparent border border-gray-300 resize-none"
                 rows={Math.max(2, fieldValues[field.name].split("\n").length)}
                 value={fieldValues[field.name]}
                 onChange={(e) => handleInputChange(field.name, e.target.value)}
@@ -80,7 +108,7 @@ export const BaseNode = ({
           }}
         />
       ))}
-      {resizable && <NodeResizer minWidth={100} minHeight={30} />}
+      {resizable && <NodeResizer minWidth={100} minHeight={30} isVisible={false} />}
     </div>
   );
 };
